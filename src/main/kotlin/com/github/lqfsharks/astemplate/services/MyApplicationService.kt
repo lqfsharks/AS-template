7ec2affbeee3c5df9e2c73c0e6c41866f3485d6d
GitHub Action <action@github.com>
2021-10-07 08:34:48 +0000

    Template cleanup

--- src/main/kotlin/com/github/lqfsharks/astemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/lqfsharks/astemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.lqfsharks.astemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lqfsharks.astemplate.MyBundle
 
 class MyApplicationService {
 
